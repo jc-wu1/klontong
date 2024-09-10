33daad4873c9b58059f13634443a7d1064e69fd5
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2024-09-10 22:36:47 +0700

    feat: add color and font conf.

---
+++ lib/src/core/design_system/texts.dart
@@ -0,0 +1,454 @@
+import 'package:flutter/material.dart';
+import '../extensions.dart';
+
+///
+/// BODY
+///
+TextStyle caption2Light = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+  fontSize: 10.0,
+  height: 14.0.lh(10),
+);
+TextStyle caption2Regular = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 10.0,
+  height: 14.0.lh(10),
+);
+TextStyle caption2Medium = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontSize: 10.0,
+  height: 14.0.lh(10),
+);
+TextStyle caption2SemiBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 10.0,
+  height: 14.0.lh(10),
+);
+TextStyle caption2Bold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 10.0,
+  height: 14.0.lh(10),
+);
+TextStyle caption2ExtraBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontSize: 37.0,
+  height: 14.0.lh(10),
+);
+TextStyle caption1Light = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+  fontSize: 12.0,
+  height: 16.8.lh(12),
+);
+TextStyle caption1Regular = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 12.0,
+  height: 16.8.lh(12),
+);
+TextStyle caption1Medium = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontSize: 12.0,
+  height: 16.8.lh(12),
+);
+TextStyle caption1SemiBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 12.0,
+  height: 16.8.lh(12),
+);
+TextStyle caption1Bold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 12.0,
+  height: 16.8.lh(12),
+);
+TextStyle caption1ExtraBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontSize: 12.0,
+  height: 16.8.lh(12),
+);
+
+///
+/// BODY
+///
+TextStyle body2Light = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+  fontSize: 14.0,
+  height: 19.6.lh(14),
+);
+TextStyle body2Regular = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 14.0,
+  height: 19.6.lh(14),
+);
+TextStyle body2Medium = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontSize: 14.0,
+  height: 19.6.lh(14),
+);
+TextStyle body2SemiBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 14.0,
+  height: 19.6.lh(14),
+);
+TextStyle body2Bold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 14.0,
+  height: 19.6.lh(14),
+);
+TextStyle body2ExtraBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontSize: 14.0,
+  height: 19.6.lh(14),
+);
+TextStyle body1Light = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+  fontSize: 16.0,
+  height: 22.4.lh(16),
+);
+TextStyle body1Regular = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 16.0,
+  height: 22.4.lh(16),
+);
+TextStyle body1Medium = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontSize: 16.0,
+  height: 22.4.lh(16),
+);
+TextStyle body1SemiBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 16.0,
+  height: 22.4.lh(16),
+);
+TextStyle body1Bold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 16.0,
+  height: 22.4.lh(16),
+);
+TextStyle body1ExtraBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontSize: 16.0,
+  height: 22.4.lh(16),
+);
+
+///
+/// SUBHEADLINE
+///
+TextStyle subheadline2Light = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+  fontSize: 18.0,
+  height: 25.2.lh(18),
+);
+TextStyle subheadline2Regular = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 18.0,
+  height: 25.2.lh(18),
+);
+TextStyle subheadline2Medium = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontSize: 18.0,
+  height: 25.2.lh(18),
+);
+TextStyle subheadline2SemiBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 18.0,
+  height: 25.2.lh(18),
+);
+TextStyle subheadline2Bold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 18.0,
+  height: 25.2.lh(18),
+);
+TextStyle subheadline2ExtraBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontSize: 18.0,
+  height: 25.2.lh(18),
+);
+TextStyle subheadline1Light = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+  fontSize: 20.0,
+  height: 28.0.lh(20),
+);
+TextStyle subheadline1Regular = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 20.0,
+  height: 28.0.lh(20),
+);
+TextStyle subheadline1Medium = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontSize: 20.0,
+  height: 28.0.lh(20),
+);
+TextStyle subheadline1SemiBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 20.0,
+  height: 28.0.lh(20),
+);
+TextStyle subheadline1Bold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 20.0,
+  height: 28.0.lh(20),
+);
+TextStyle subheadline1ExtraBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontSize: 20.0,
+  height: 28.0.lh(20),
+);
+
+///
+/// HEADLINE
+///
+TextStyle headline4Light = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+  fontSize: 24.0,
+  height: 28.8.lh(24),
+);
+TextStyle headline4Regular = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 24.0,
+  height: 28.8.lh(24),
+);
+TextStyle headline4Medium = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontSize: 24.0,
+  height: 28.8.lh(24),
+);
+TextStyle headline4SemiBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 24.0,
+  height: 28.8.lh(24),
+);
+TextStyle headline4Bold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 24.0,
+  height: 28.8.lh(24),
+);
+TextStyle headline4ExtraBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontSize: 24.0,
+  height: 28.8.lh(24),
+);
+TextStyle headline3Light = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+  fontSize: 28.0,
+  height: 33.6.lh(28),
+);
+TextStyle headline3Regular = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 28.0,
+  height: 33.6.lh(28),
+);
+TextStyle headline3Medium = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontSize: 28.0,
+  height: 33.6.lh(28),
+);
+TextStyle headline3SemiBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 28.0,
+  height: 33.6.lh(28),
+);
+TextStyle headline3Bold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 28.0,
+  height: 33.6.lh(28),
+);
+TextStyle headline3ExtraBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontSize: 28.0,
+  height: 33.6.lh(28),
+);
+TextStyle headline2Light = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+  fontSize: 32.0,
+  height: 38.4.lh(32),
+);
+TextStyle headline2Regular = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 32.0,
+  height: 38.4.lh(32),
+);
+TextStyle headline2Medium = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontSize: 32.0,
+  height: 38.4.lh(32),
+);
+TextStyle headline2SemiBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 32.0,
+  height: 38.4.lh(32),
+);
+TextStyle headline2Bold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 32.0,
+  height: 38.4.lh(32),
+);
+TextStyle headline2ExtraBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontSize: 32.0,
+  height: 38.4.lh(32),
+);
+TextStyle headline1Light = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+  fontSize: 40.0,
+  height: 44.0.lh(40),
+);
+TextStyle headline1Regular = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 40.0,
+  height: 44.0.lh(40),
+);
+TextStyle headline1Medium = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontSize: 40.0,
+  height: 44.0.lh(40),
+);
+TextStyle headline1SemiBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 40.0,
+  height: 44.0.lh(40),
+);
+TextStyle headline1Bold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 40.0,
+  height: 44.0.lh(40),
+);
+TextStyle headline1ExtraBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontSize: 40.0,
+  height: 44.0.lh(40),
+);
+
+///
+/// DISPLAY
+///
+TextStyle display2Light = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+  fontSize: 48.0,
+  height: 52.8.lh(48),
+);
+TextStyle display2Regular = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 48.0,
+  height: 52.8.lh(48),
+);
+TextStyle display2Medium = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontSize: 48.0,
+  height: 52.8.lh(48),
+);
+TextStyle display2SemiBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 48.0,
+  height: 52.8.lh(48),
+);
+TextStyle display2Bold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 48.0,
+  height: 52.8.lh(48),
+);
+TextStyle display2ExtraBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontSize: 48.0,
+  height: 52.8.lh(48),
+);
+TextStyle display1Light = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w300,
+  fontSize: 56.0,
+  height: 61.6.lh(56),
+);
+TextStyle display1Regular = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w400,
+  fontSize: 56.0,
+  height: 61.6.lh(56),
+);
+TextStyle display1Medium = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontSize: 56.0,
+  height: 61.6.lh(56),
+);
+TextStyle display1SemiBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w600,
+  fontSize: 56.0,
+  height: 61.6.lh(56),
+);
+TextStyle display1Bold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 56.0,
+  height: 61.6.lh(56),
+);
+TextStyle display1ExtraBold = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w800,
+  fontSize: 56.0,
+  height: 61.6.lh(56),
+);
