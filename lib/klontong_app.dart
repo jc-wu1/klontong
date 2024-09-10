3db59db7672af85f3f00bceed04a9595233e3c58
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2024-09-10 17:03:08 +0700

    WIP

---
+++ lib/klontong_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'src/features/home/presentation/home_page.dart';
+
+class KlontongApp extends StatelessWidget {
+  const KlontongApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Klontong Mobile Apps',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
