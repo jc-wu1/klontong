30ac0b925e6d27b81f5a38982027ca913bb321d2
Julian Wu <34271521+jc-wu1@users.noreply.github.com>
2024-09-11 16:53:15 +0700

    update: add assets variables

---
+++ lib/src/core/assets/asset_images.dart
@@ -0,0 +1,6 @@
+const _assetsBanners = 'assets/images/banners';
+
+const String bannerpromo1 = '$_assetsBanners/promo1.png';
+const String bannerpromo2 = '$_assetsBanners/promo2.png';
+const String bannerpromo3 = '$_assetsBanners/promo3.png';
+const String bannerpromo4 = '$_assetsBanners/promo4.png';
